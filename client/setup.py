70abf4fac3354af07bba83226bf815f2c98b94f0
Robert McGibbon <rmcgibo@gmail.com>
2012-11-20 11:04:50 -0800

    minor fixes

--- client/setup.py
+++ client/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(name='fsync-client',
       version='0.1',
-      install_requires=['pyzmq'],
+      install_requires=['pyzmq', 'pexpect'],
       license='GPL-3',
       author='Robert McGibbon',
       author_email='rmcgibbo@gmail.com',
