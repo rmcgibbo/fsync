f4c72f6f3762443fb3ffbae6c21a68387a88f113
Robert McGibbon <rmcgibo@gmail.com>
2012-11-20 10:32:50 -0800

    made client script more robust, give usage info, look for different editors based on flags or env vars

---
+++ client/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='fsync-client',
+      version='0.1',
+      install_requires=['pyzmq'],
+      license='GPL-3',
+      author='Robert McGibbon',
+      author_email='rmcgibbo@gmail.com',
+      scripts=['fs'])
+
